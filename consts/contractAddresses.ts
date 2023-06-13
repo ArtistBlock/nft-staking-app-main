4bec900909ac13e6f8726460959643ca35e02320
ArtistBlock <blocksprime@gmail.com>
2023-06-13 10:15:17 +0200

    first commit

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x8D99c4567609c8487b8B84bfe3687a8221EDa860";
+export const tokenContractAddress =
+  "0xB79d5954b9c02eE5467023d26140F94f9b01AA7b";
+export const stakingContractAddress =
+  "0x07Ee6F27D4EFC829947CD46F19587E7003D46748";
